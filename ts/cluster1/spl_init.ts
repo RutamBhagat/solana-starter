fc1901a436978f22e231467425a615a1e9117a46
RutamBhagat <rutambhagat@gmail.com>
2024-07-31 01:58:37 +0530

    complete spl token funcs

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,7 @@
-import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import { Commitment, Connection, Keypair } from "@solana/web3.js";
+
+import { createMint } from "@solana/spl-token";
+import wallet from "../wba-wallet.json";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,10 +11,11 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 (async () => {
-    try {
-        // Start here
-        // const mint = ???
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+  try {
+    // Start here
+    const mint = await createMint(connection, keypair, keypair.publicKey, keypair.publicKey, 6);
+    console.log(`https://explorer.solana.com/address/${mint}?cluster=devnet`);
+  } catch (error) {
+    console.log(`Oops, something went wrong: ${error}`);
+  }
+})();
