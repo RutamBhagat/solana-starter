ef526108e1b22ef2f3e6b4942d0d59dfc0334e12
RutamBhagat <rutambhagat@gmail.com>
2024-08-01 20:48:12 +0530

    complete nft section

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,11 +1,12 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
-import { readFile } from "fs/promises"
+import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi";
+
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFile } from "fs/promises";
+import wallet from "../wba-wallet.json";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
@@ -14,17 +15,19 @@ umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        //1. Load image
-        //2. Convert image to generic file.
-        //3. Upload image
+  try {
+    //1. Load image
+    const imageFile = await readFile(
+      "/home/rutam/Downloads/Code/rust/turbin_wba/week_0/solana-starter/ts/cluster1/generug.png"
+    );
 
-        // const image = ???
+    //2. Convert image to generic file.
+    const generic = createGenericFile(imageFile, "generug.png", { contentType: "image/png" });
 
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    //3. Upload image
+    const [myUri] = await umi.uploader.upload([generic]);
+    console.log("Image uploaded successfully. URI:", myUri);
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -1,9 +1,9 @@
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { createSignerFromKeypair, generateSigner, percentAmount, signerIdentity } from "@metaplex-foundation/umi";
 
-import wallet from "../wba-wallet.json"
 import base58 from "bs58";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import wallet from "../wba-wallet.json";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
 const umi = createUmi(RPC_ENDPOINT);
@@ -11,16 +11,24 @@ const umi = createUmi(RPC_ENDPOINT);
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const myKeypairSigner = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(myKeypairSigner));
-umi.use(mplTokenMetadata())
+umi.use(mplTokenMetadata());
 
 const mint = generateSigner(umi);
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+  let tx = await createNft(umi, {
+    mint,
+    name: "My NFT",
+    symbol: "MNFT",
+    uri: "https://arweave.net/Z5b0o6A6XwOSUPST9BzzR_-CefqkvUvM9fvw60StwJo",
+    sellerFeeBasisPoints: percentAmount(5, 2), // 5%
+  });
 
-    console.log("Mint Address: ", mint.publicKey);
+  let result = await tx.sendAndConfirm(umi);
+  const signature = base58.encode(result.signature);
+
+  console.log(
+    `Successfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`
+  );
+  console.log("Mint Address: ", mint.publicKey);
 })();
